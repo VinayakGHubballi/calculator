59c3a81b0d6377868fb19819d237fcc3b9a0d154
vinayak.g.hubballi <vinayak.g.hubballi@gmail.com>
2026-08-17 22:23:27 +0530

    Saving new calc in new branch local

--- calc.py
+++ calc.py
@@ -5,5 +5,5 @@ print("Addition: ", a+b);
 print("Subtraction: ", a-b);
 print("Multiplication: ", a*b);
 print("Division: ", a/b);
-print("Modified this line in local. When pushed, this change will be visible in github");
-print("This line is modified in github repository. When pull, this change will be visible in local");
+print("-----Adding modulous to new branch-----");
+print("Mod: ", a%b);
